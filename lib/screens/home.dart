5071c571c378d0e5844ba6e8bbeba068bc186ebf
Oli <dev.olivercarlos@gmail.com>
2019-10-02 19:23:41 +0800

    Use of FutureBuilder.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,41 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {  
-  List<String> _pageData;
-  bool get _fetchingData => _pageData == null;
-
-  @override
-  initState() {
-    // You set condition here if there is data being fetched.
-    // or if there is an error occured.
-    _getListData().then((data) => setState(() {
-      if (data.length == 0) {
-        data.add("No data found for your account.");
-      }
-      _pageData = data;
-    })).catchError((error) => setState(() {
-      _pageData = [error];
-    }));
-    super.initState();
-  }
-
-  Widget build(BuildContext build) {
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      body: _fetchingData ? Center(child: CircularProgressIndicator()) : 
-      ListView.builder(
-        itemCount: _pageData.length,
-        itemBuilder: (buildContext, index) => _getListItemUi(index),
-      )
-    );
+        backgroundColor: Colors.grey[900],
+        body: FutureBuilder(
+            future: _getListData(),
+            builder: (buildContext, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              var listItems = snapshot.data;
+              return ListView.builder(
+                itemCount: listItems.length,
+                itemBuilder: (buildContext, index) =>
+                    _getListItemUi(index, listItems),
+              );
+            }));
   }
 
-  Future<List<String>> _getListData({bool hasError = false, bool hasData = true}) async {
+  Future<List<String>> _getListData(
+      {bool hasError = false, bool hasData = true}) async {
     await Future.delayed(Duration(seconds: 2));
     if (hasError) {
       return Future.error("An error has occured please try again.");
@@ -48,16 +33,25 @@ class _HomeState extends State<Home> {
     }
   }
 
-  Widget _getListItemUi(int index) {
+  Widget _getListItemUi(int index, List<String> listItems) {
     return Container(
-      margin: EdgeInsets.all(5.0),
-      height: 50.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5.0),
-        color: Colors.grey[600]
-      ),
-      child: Center(child: Text(_pageData[index], style: TextStyle(color: Colors.white,)))
-    );
+        margin: EdgeInsets.all(5.0),
+        height: 50.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5.0), color: Colors.grey[600]),
+        child: Center(
+            child: Text(listItems[index],
+                style: TextStyle(
+                  color: Colors.white,
+                ))));
   }
 
+  Widget _getInformationMessage(String message) {
+    return Center(
+      child: Text(message,
+          textAlign: TextAlign.center,
+          style:
+              TextStyle(fontWeight: FontWeight.w900, color: Colors.grey[500])),
+    );
+  }
 }
